94a48f6e6cec3a8631545ea771576324a94ef71a
Aminbaxxx <aminbaxxxx@gmail.com>
2025-02-11 21:58:02 +0330

    Add responsive design, implement hamburger menu, and fix bugs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,75 @@
 "use strict";
-const darkKey = document.querySelector(".dark-mode");
-const darkKeyChanger = document.querySelector(".dark-mode use");
+const darkKey = document.querySelectorAll(".dark-mode");
+const closeIcon = document.querySelector(".close-icon");
+const modal = document.querySelector("#modal");
+const openSubMenu = document.querySelector(".open-submenu");
+const openSubMenuHeight = document.querySelector(".open-submenu-height");
+const overlay = document.querySelector(".overlay");
+const hambergerMenu = document.querySelector("#hamberger-menu");
+const darkKeyChanger = document.querySelectorAll(".dark-mode use");
+const nameChanger = document.querySelector(".name-changer");
 // Run For If clinet is true or false in he/she localstorage
 if (localStorage.getItem("dark-mode") === null) {
   localStorage.setItem("dark-mode", "false");
 }
 if (localStorage.getItem("dark-mode") === "true") {
   document.documentElement.className = "dark";
-  darkKeyChanger.setAttribute("href", "#moon");
+
+  darkKeyChanger.forEach((key) => {
+    key.setAttribute("href", "#moon");
+    if (key.dataset.set === "theme") {
+      key.setAttribute("href", "#sun");
+      nameChanger.textContent = "تم روشن";
+    }
+  });
 } else {
   document.documentElement.className = "light";
   localStorage.setItem("dark-mode", "false");
-  darkKeyChanger.setAttribute("href", "#sun");
+  darkKeyChanger.forEach((key) => {
+    key.setAttribute("href", "#sun");
+    if (key.dataset.set === "theme") {
+      key.setAttribute("href", "#moon");
+      nameChanger.textContent = "تم تاریک";
+    }
+  });
 }
-//
-darkKey.addEventListener("click", () => {
-  if (localStorage.getItem("dark-mode") === "false") {
-    document.documentElement.className = "dark";
-    darkKeyChanger.setAttribute("href", "#moon");
-
-    localStorage.setItem("dark-mode", "true");
-  } else {
-    document.documentElement.className = "light";
-    localStorage.setItem("dark-mode", "false");
-    darkKeyChanger.setAttribute("href", "#sun");
-  }
+// addEvent Listener
+darkKey.forEach((allkey) => {
+  allkey.addEventListener("click", () => {
+    if (localStorage.getItem("dark-mode") === "false") {
+      console.log("check IF");
+      localStorage.setItem("dark-mode", "true");
+      document.documentElement.className = "dark";
+      nameChanger.textContent = "تم روشن";
+      darkKeyChanger.forEach((key) => {
+        key.setAttribute("href", "#moon");
+        if (key.dataset.set === "theme") {
+          key.setAttribute("href", "#sun");
+        }
+      });
+    } else {
+      document.documentElement.className = "light";
+      localStorage.setItem("dark-mode", "false");
+      nameChanger.textContent = "تم تاریک";
+      darkKeyChanger.forEach((key) => {
+        key.setAttribute("href", "#sun");
+        if (key.dataset.set === "theme") {
+          key.setAttribute("href", "#moon");
+        }
+      });
+    }
+  });
+});
+//Close Icon
+closeIcon.addEventListener("click", () => {
+  modal.style.transform = "translateX(349px)";
+  overlay.style.display = "none";
+});
+hambergerMenu.addEventListener("click", () => {
+  modal.style.transform = "translateX(0px)";
+  overlay.style.display = "block";
+});
+overlay.addEventListener("click", () => {
+  overlay.style.display = "none";
+  modal.style.transform = "translateX(349px)";
 });
